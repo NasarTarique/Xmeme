979cc800a81986914dd9f1a21fd33003ad5ac25d
NasarTarique <nasartarique@gmail.com>
2021-02-19 21:12:04 +0530

    update install.sh

--- install.sh
+++ install.sh
@@ -3,19 +3,12 @@
 dbname="xmeme"
 username="hp"
 
-sudo apt install -y curl
-
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-sudo apt-get install -y nodejs
-
-sudo apt install -y python3-pip
-pip3 install -r requirements.txt
-
+pip3 install -r requirements.txt -q
 sudo apt-get install -y postgresql postgresql-contrib
 
 sudo apt-get install -y libpq-dev python3-dev
 
-sudo -u postgres psql <<EOF
+sudo -u postgres psql -q <<EOF
 CREATE DATABASE	$dbname;
 CREATE USER $username WITH ENCRYPTED PASSWORD 'regulus';
 ALTER ROLE $username SET client_encoding TO 'utf8';
@@ -23,9 +16,10 @@ ALTER ROLE $username SET default_transaction_isolation TO 'read committed';
 ALTER ROLE $username SET timezone TO 'UTC';
 GRANT ALL PRIVILEGES ON DATABASE $dbname TO $username;
 EOF
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
 
 cd ./frontend/
-echo "$(pwd)"
 npm install
 cd ../
-echo "$(pwd)"
